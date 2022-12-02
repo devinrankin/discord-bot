f7f2d8d1127d3cc9dfe96b47fb8ff21507e9a316
devinrankin <devinrankin921@gmail.com>
2022-12-02 07:58:57 -0500

    using api calls to run bot and send messages

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import commands
+
+async def send_message(message, user_message):
+    try:
+        response = commands.handle_command(user_message)
+        await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_bot():
+    print(discord.__version__)
+    TOKEN = 'MTA0ODE5NzcwMzI4MjIxNjk3MA.GFqxx-.vdzM2Op4Oq32Jo7RoguDmrCXFlTBDCh2z8sA90'
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}.')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said {user_message} in ({channel}).')
+
+        await send_message(message, user_message)
+    
+    client.run(TOKEN)
